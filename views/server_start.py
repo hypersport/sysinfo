20cdfcb6b601fcf0fa2dbc0e0a2f809d5bd3d7e8
qiansen <boss.yuan@outlook.com>
2017-03-12 16:10:48 +0800

    CPU 后台及页面数据展示

--- views/server_start.py
+++ views/server_start.py
@@ -34,8 +34,32 @@ def index():
 
 @main.route('/cpu')
 def cpu_info():
+    logical_core_num = psutil.cpu_count()
+    physical_core_num = psutil.cpu_count(logical=False)
     load_avg = os.getloadavg()
+    cpu_time_percent = psutil.cpu_times_percent()
+    user_percent = cpu_time_percent[0]
+    sys_percent = cpu_time_percent[2]
+    idle_percent = cpu_time_percent[3]
+    iowait_percent = cpu_time_percent[4]
+    nice_percent = cpu_time_percent[1]
+    else_percent = 0.0
+    for i in range(5, 10):
+        else_percent += cpu_time_percent[i]
+    run_freq = round(psutil.cpu_freq()[0] / 1000, 1)
+    min_freq = round(psutil.cpu_freq()[1] / 1000, 1)
+    max_freq = round(psutil.cpu_freq()[2] / 1000, 1)
     return render_template('cpu.html',
-                           load_avg=load_avg
+                           physical_core_num=physical_core_num,
+                           logical_core_num=logical_core_num,
+                           load_avg=load_avg,
+                           user_percent=user_percent,
+                           sys_percent=sys_percent,
+                           idle_percent=idle_percent,
+                           iowait_percent=iowait_percent,
+                           nice_percent=nice_percent,
+                           else_percent=nice_percent,
+                           run_freq=run_freq,
+                           min_freq=min_freq,
+                           max_freq=max_freq
                            )
-
