3a7b615decaf16277294eac58ed80cef4d8052ae
qiansen <boss.yuan@outlook.com>
2017-03-16 20:50:03 +0800

    将自定义的过滤器注册到 app

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 # coding=utf-8
 from flask import Flask
 from views import main
-from views.custom_filters import format_time, format_size
+from views.custom_filters import format_time, format_size, socket_type, socket_family, format_addr_port
 
 app = Flask(__name__)
 app.add_template_filter(format_time)
 app.add_template_filter(format_size)
+app.add_template_filter(socket_type)
+app.add_template_filter(socket_family)
+app.add_template_filter(format_addr_port)
 app.register_blueprint(main)
 if __name__ == '__main__':
     app.run(port=9468)
