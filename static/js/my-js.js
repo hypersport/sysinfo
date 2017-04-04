ff8f79df006bba078971d9ef2d1735cfe8acc16a
qiansen <boss.yuan@outlook.com>
2017-04-04 14:26:07 +0800

    磁盘页面样式更新

--- static/js/my-js.js
+++ static/js/my-js.js
@@ -2,16 +2,43 @@
  * Created by root on 17-3-30.
  */
 $(document).ready(function () {
+    // for memory.html
     $("#swap-span").click(function () {
         $("#memory-span").css("color", "#cccccc");
         $("#swap-span").css("color", "#000000");
-        $(".memory").hide();
-        $(".swap-info").show();
+        $("#memory").hide();
+        $("#swap-info").show();
     });
     $("#memory-span").click(function () {
         $("#swap-span").css("color", "#cccccc");
         $("#memory-span").css("color", "#000000");
-        $(".swap-info").hide();
-        $(".memory").show();
+        $("#swap-info").hide();
+        $("#memory").show();
+    });
+
+    // for disks.html
+    $("#disk-span").click(function () {
+        $("#disk-span").css("color", "#000000");
+        $("#part-span").css("color", "#cccccc");
+        $("#disk-io-span").css("color", "#cccccc");
+        $("#parts").hide();
+        $("#disk-io").hide();
+        $("#disks").show();
+    });
+    $("#part-span").click(function () {
+        $("#disk-io-span").css("color", "#cccccc");
+        $("#disk-span").css("color", "#cccccc");
+        $("#part-span").css("color", "#000000");
+        $("#disks").hide();
+        $("#disk-io").hide();
+        $("#parts").show();
+    });
+    $("#disk-io-span").click(function () {
+        $("#part-span").css("color", "#cccccc");
+        $("#disk-span").css("color", "#cccccc");
+        $("#disk-io-span").css("color", "#000000");
+        $("#disks").hide();
+        $("#parts").hide();
+        $("#disk-io").show();
     });
 });
