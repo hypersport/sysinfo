060ef9b83093abd2110fb24984330e782aa8eb91
qiansen <boss.yuan@outlook.com>
2017-04-04 15:32:09 +0800

    使用 JQuery 过滤选择元素

--- static/js/my-js.js
+++ static/js/my-js.js
@@ -1,58 +1,49 @@
-/**
- * Created by root on 17-3-30.
- */
 $(document).ready(function () {
     // for memory.html
-    $("#swap-span").click(function () {
-        $("#memory-span").css("color", "#cccccc");
-        $("#swap-span").css("color", "#000000");
-        $("#memory").hide();
-        $("#swap-info").show();
-    });
     $("#memory-span").click(function () {
-        $("#swap-span").css("color", "#cccccc");
         $("#memory-span").css("color", "#000000");
-        $("#swap-info").hide();
+        $("#memory-span").siblings().css("color", "#cccccc");
+        $("#memory").siblings().hide();
         $("#memory").show();
+    });
+    $("#swap-span").click(function () {
+        $("#swap-span").css("color", "#000000");
+        $("#swap-span").siblings().css("color", "#cccccc");
+        $("#swap-info").siblings().hide();
+        $("#swap-info").show();
     });
 
     // for disks.html
     $("#disk-span").click(function () {
         $("#disk-span").css("color", "#000000");
-        $("#part-span").css("color", "#cccccc");
-        $("#disk-io-span").css("color", "#cccccc");
-        $("#parts").hide();
-        $("#disk-io").hide();
+        $("#disk-span").siblings().css("color", "#cccccc");
+        $("#disks").siblings().hide();
         $("#disks").show();
     });
     $("#part-span").click(function () {
-        $("#disk-io-span").css("color", "#cccccc");
-        $("#disk-span").css("color", "#cccccc");
         $("#part-span").css("color", "#000000");
-        $("#disks").hide();
-        $("#disk-io").hide();
+        $("#part-span").siblings().css("color", "#cccccc");
+        $("#parts").siblings().hide();
         $("#parts").show();
     });
     $("#disk-io-span").click(function () {
-        $("#part-span").css("color", "#cccccc");
-        $("#disk-span").css("color", "#cccccc");
         $("#disk-io-span").css("color", "#000000");
-        $("#disks").hide();
-        $("#parts").hide();
+        $("#disk-io-span").siblings().css("color", "#cccccc");
+        $("#disk-io").siblings().hide();
         $("#disk-io").show();
     });
 
     // for network.html
     $("#network-span").click(function () {
-        $("#connections-span").css("color", "#cccccc");
         $("#network-span").css("color", "#000000");
-        $("#connections-info").hide();
+        $("#network-span").siblings().css("color", "#cccccc");
+        $("#network-info").siblings().hide();
         $("#network-info").show();
     });
     $("#connections-span").click(function () {
-        $("#network-span").css("color", "#cccccc");
         $("#connections-span").css("color", "#000000");
-        $("#network-info").hide();
+        $("#connections-span").siblings().css("color", "#cccccc");
+        $("#connections-info").siblings().hide();
         $("#connections-info").show();
     });
 });
