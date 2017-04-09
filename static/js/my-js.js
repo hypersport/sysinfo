9276f413131556f4fd1a42a5afe2e810a91472b3
qiansen <boss.yuan@outlook.com>
2017-04-09 21:42:04 +0800

    修改 js 自动刷新

--- static/js/my-js.js
+++ static/js/my-js.js
@@ -1,49 +1,18 @@
 $(document).ready(function () {
-    // for memory.html
-    $("#memory-span").click(function () {
-        $("#memory-span").css("color", "#000000");
-        $("#memory-span").siblings().css("color", "#cccccc");
-        $("#memory").siblings().hide();
-        $("#memory").show();
-    });
-    $("#swap-span").click(function () {
-        $("#swap-span").css("color", "#000000");
-        $("#swap-span").siblings().css("color", "#cccccc");
-        $("#swap-info").siblings().hide();
-        $("#swap-info").show();
-    });
-
-    // for disks.html
-    $("#disk-span").click(function () {
-        $("#disk-span").css("color", "#000000");
-        $("#disk-span").siblings().css("color", "#cccccc");
-        $("#disks").siblings().hide();
-        $("#disks").show();
-    });
-    $("#part-span").click(function () {
-        $("#part-span").css("color", "#000000");
-        $("#part-span").siblings().css("color", "#cccccc");
-        $("#parts").siblings().hide();
-        $("#parts").show();
-    });
-    $("#disk-io-span").click(function () {
-        $("#disk-io-span").css("color", "#000000");
-        $("#disk-io-span").siblings().css("color", "#cccccc");
-        $("#disk-io").siblings().hide();
-        $("#disk-io").show();
-    });
-
-    // for network.html
-    $("#network-span").click(function () {
-        $("#network-span").css("color", "#000000");
-        $("#network-span").siblings().css("color", "#cccccc");
-        $("#network-info").siblings().hide();
-        $("#network-info").show();
-    });
-    $("#connections-span").click(function () {
-        $("#connections-span").css("color", "#000000");
-        $("#connections-span").siblings().css("color", "#cccccc");
-        $("#connections-info").siblings().hide();
-        $("#connections-info").show();
-    });
+    setInterval(refresh, 3000);
 });
+// auto refresh page
+var autoRefresh = false;
+function refresh() {
+    if (autoRefresh) {
+        return;
+    }
+    $.ajax({
+        url: location.href,
+        cache: false,
+        dataType: "html",
+        success: function (resp) {
+            $(".sysinfo").html(resp);
+        }
+    });
+}
